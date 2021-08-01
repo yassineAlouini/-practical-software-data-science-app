5503f3eb41b779a7ee4f83d2f04954b3fbb85602
Yassine Alouini <yassinealouini@outlook.com>
2021-08-01 11:20:51 +0200

    Add an app mention to the sidebar.

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ if uploaded_img_file is not None:
     img_bytes = uploaded_img_file.read()
 
     st.image(img_bytes)
+
+    markdown = """
+    A [streamlit](https://streamlit.io/) app by [Yassine](https://www.kaggle.com/yassinealouini).
+    The source code could be found [here](https://github.com/yassineAlouini/practical-software-data-science-app).
+    """
+    st.sidebar.markdown(markdown)
